c3a8175e8bad737b3e7f09f0efa42ea01358ca4e
NewAmazingPVP <gamingofpat@gmail.com>
2023-05-06 12:10:53 -0400

    Working Updating all plugins
    Working Updating all plugins

--- src/main/java/spigot/SpigotUpdate.java
+++ src/main/java/spigot/SpigotUpdate.java
@@ -1,60 +1,46 @@
 package spigot;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import common.UpdatePlugins;
-import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
 
 public final class SpigotUpdate extends JavaPlugin {
 
     private UpdatePlugins m_updatePlugins;
+    private File myFile;
 
     @Override
     public void onEnable() {
-        String spigotResourceLink = "https://www.spigotmc.org/resources/view-distance-tweaks-1-14-1-19.75164/";
-        String pluginId = m_updatePlugins.extractPluginIdFromLink(spigotResourceLink);
-        String downloadUrl = "https://api.spiget.org/v2/resources/" + pluginId + "/download";
-        System.out.println("Download URL: " + downloadUrl);
-        m_updatePlugins.updateFloodgate(downloadUrl);
-        listYML();
-        readList();
-    }
-
-    public void listYML() {
+        m_updatePlugins = new UpdatePlugins();
         File dataFolder = getDataFolder();
 
-        // Create the data folder if it doesn't exist
         if (!dataFolder.exists()) {
             dataFolder.mkdirs();
         }
 
-        File myFile = new File(dataFolder, "list.yml");
-        try {
-            if (!myFile.exists()) {
+        myFile = new File(dataFolder, "list.yml");
+        if (!myFile.exists()) {
+            try {
                 myFile.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        periodUpdatePlugins();
     }
 
-    public void readList() {
-        Yaml yaml = new Yaml();
-        try (FileReader reader = new FileReader("links.yml")) {
-            Map<String, String> links = yaml.load(reader);
-            for (Map.Entry<String, String> entry : links.entrySet()) {
-                getLogger().info(entry.getKey() + " ---- " + entry.getValue());
+    public void periodUpdatePlugins(){
+        Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
+            @Override
+            public void run(){
+                m_updatePlugins.readList(myFile);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        }, 0L, 20L * 60L * 1);
     }
-
-
 }
 
 
