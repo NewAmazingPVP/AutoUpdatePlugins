784746489b47ecf9359c2284020790a2832f1106
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-05-07 11:57:42 -0400

    Aysnc taks for performance improvement
    Aysnc taks for performance improvement

--- src/main/java/spigot/SpigotUpdate.java
+++ src/main/java/spigot/SpigotUpdate.java
@@ -34,13 +34,14 @@ public final class SpigotUpdate extends JavaPlugin {
     public void periodUpdatePlugins(){
         int interval = config.getInt("updates.interval");
         long bootTime = config.getInt("updates.bootTime");
-        Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(this, new Runnable() {
             @Override
             public void run(){
                 m_updatePlugins.readList(myFile);
             }
         }, bootTime, 20L * 60L * interval);
     }
+
 }
 
 
