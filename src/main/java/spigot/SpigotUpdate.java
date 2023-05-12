4e02996c7f9d89c632f603dad90af5680b5a62ea
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-05-11 22:13:24 -0400

    added new library
    added new library

--- src/main/java/spigot/SpigotUpdate.java
+++ src/main/java/spigot/SpigotUpdate.java
@@ -4,6 +4,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import common.UpdatePlugins;
+import org.inventivetalent.update.spiget.SpigetUpdate;
+import org.inventivetalent.update.spiget.UpdateCallback;
+import org.inventivetalent.update.spiget.comparator.VersionComparator;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +16,7 @@ public final class SpigotUpdate extends JavaPlugin {
     private UpdatePlugins m_updatePlugins;
     private File myFile;
     private FileConfiguration config;
+    private final File pluginFolder = new File(getDataFolder().getParentFile(), "plugins");
 
     @Override
     public void onEnable() {
@@ -29,6 +33,7 @@ public final class SpigotUpdate extends JavaPlugin {
             }
         }
         periodUpdatePlugins();
+        checkForUpdates();
     }
 
     public void periodUpdatePlugins(){
@@ -42,6 +47,45 @@ public final class SpigotUpdate extends JavaPlugin {
         }, bootTime, 20L * 60L * interval);
     }
 
+    private void checkForUpdates() {
+        for (File file : pluginFolder.listFiles()) {
+            if (file.isFile() && file.getName().endsWith(".jar")) {
+                SpigetUpdate updater = new SpigetUpdate(this, 334543);
+                updater.checkForUpdate(new UpdateCallback() {
+                    @Override
+                    public void updateAvailable(String newVersion, String downloadUrl, boolean hasDirectDownload) {
+                        if (hasDirectDownload) {
+                            getLogger().info("Updating " + file.getName() + " to version " + newVersion);
+                            if (updater.downloadUpdate()) {
+                                getLogger().info("Update for " + file.getName() + " downloaded successfully");
+                            } else {
+                                getLogger().warning("Update for " + file.getName() + " failed: " + updater.getFailReason());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void upToDate() {
+                        getLogger().info(file.getName() + " is up-to-date");
+                    }
+                });
+            }
+        }
+    }
+
+
+
+    private int getPluginId(File file) {
+        String name = file.getName();
+        int index = name.indexOf('-');
+        if (index > 0) {
+            try {
+                return Integer.parseInt(name.substring(0, index));
+            } catch (NumberFormatException e) {
+            }
+        }
+        return -1;
+    }
 }
 
 
