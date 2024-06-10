9ec06dd66f575c0aeadde2f6f8d8a95e667dd308
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-06-10 19:21:36 -0400

    multi artifacts and papermc hangar support
    multi artifacts and papermc hangar support

--- src/main/java/bungeecord/BungeeUpdate.java
+++ src/main/java/bungeecord/BungeeUpdate.java
@@ -48,7 +48,7 @@ public final class BungeeUpdate extends Plugin {
         getProxy().getScheduler().schedule(this, () -> {
             getProxy().getScheduler().runAsync(this, () -> {
                 try {
-                    m_updatePlugins.readList(myFile, "bungeecord");
+                    m_updatePlugins.readList(myFile, "waterfall");
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
@@ -85,7 +85,7 @@ public final class BungeeUpdate extends Plugin {
         @Override
         public void execute(CommandSender sender, String[] args) {
             try {
-                m_updatePlugins.readList(myFile, "bungeecord");
+                m_updatePlugins.readList(myFile, "waterfall");
                 sender.sendMessage(ChatColor.AQUA + "Plugins are successfully updating!");
             } catch (IOException e) {
                 sender.sendMessage(ChatColor.RED + "Plugins failed to update!");
