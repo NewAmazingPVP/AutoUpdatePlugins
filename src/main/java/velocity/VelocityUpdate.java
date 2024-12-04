dd204e31199b6b31dba232ca2303e2a4f678115f
NewAmazingPVP <gamingofpat@gmail.com>
2024-12-03 21:49:04 -0500

    v10.5 release (full jenkins + pre release + update folder, etc support added)
    v10.5 release (full jenkins + pre release + update folder, etc support added)

--- src/main/java/velocity/VelocityUpdate.java
+++ src/main/java/velocity/VelocityUpdate.java
@@ -23,7 +23,7 @@ import java.nio.file.Path;
 import java.time.Duration;
 import java.util.logging.Logger;
 
-@Plugin(id = "autoupdateplugins", name = "AutoUpdatePlugins", version = "10.0", url = "https://www.spigotmc.org/resources/autoupdateplugins.109683/", authors = "NewAmazingPVP")
+@Plugin(id = "autoupdateplugins", name = "AutoUpdatePlugins", version = "10.5", url = "https://www.spigotmc.org/resources/autoupdateplugins.109683/", authors = "NewAmazingPVP")
 public final class VelocityUpdate {
 
     private PluginUpdater pluginUpdater;
